3a7044434353a74c4e64f8d4cc4d76ffe538726e
AlyMagged <alimaged98@gmail.com>
2020-09-16 22:38:52 +0200

    Migrate Machine Models to MongoDB database

--- src/ecommerce/models.py
+++ src/ecommerce/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+
 class MachineProductType(models.Model):
     name = models.CharField(max_length=50)
+    code = models.CharField(max_length=10, null=True)
+
+
+class MachineModel(models.Model):
+    name = models.CharField(max_length=50)
+    product_type_id = models.IntegerField()
+    code = models.CharField(max_length=10, null=True)
+
+
+class MachineWaterLine(models.Model):
+    type = models.BooleanField(default=True)
+    model_id = models.IntegerField()
 
