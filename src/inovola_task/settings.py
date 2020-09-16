32f7a5ab3e3acc62638c62066385fa6b943bbd1b
AlyMagged <alimaged98@gmail.com>
2020-09-16 20:58:10 +0200

    add ecommerce project

--- src/inovola_task/settings.py
+++ src/inovola_task/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,20 @@ WSGI_APPLICATION = 'inovola_task.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'bezkoder_db',
+        'HOST': '127.0.0.1',
+        'PORT': 27017,
     }
 }
 
